5980bb134072848676a4d2e052a6ec02d335df1d
loser-hao <912056802@qq.com>
2021-01-05 22:25:19 +0800

    后台框架搭建

--- assets/js/ajaxRoot.js
+++ assets/js/ajaxRoot.js
@@ -1,4 +1,27 @@
 // url的优化
 $.ajaxPrefilter(function (params) {
     params.url = "http://api-breakingnews-web.itheima.net" + params.url;
+
+    if(params.url.indexOf("/my") !== -1 ){
+      params.headers ={Authorization: localStorage.getItem("token")} 
+    }
+
+      // 控制用户访问权限
+      params.complete = function (xhr) {
+    // 请求完成就会执行的函数（不论是失败还是成功都会执行的）
+    // 形参可以获取到xhr对象
+    //   console.log(xhr);
+    if (
+      xhr.responseJSON.status === 1 &&
+      xhr.responseJSON.message === "身份认证失败！"
+    ) {
+      // 回到登录页面重新登录
+      // 把token也清除掉
+      localStorage.removeItem("token");
+      location.href = "login.html";
+    }
+  };
+   
   })
+
+  
---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+// 获取头像和昵称
+function getNameAndPortrait(){
+console.log(localStorage.getItem("token"));
+$.ajax({
+    url: "/my/userinfo",
+    // headers: {
+    //     Authorization: localStorage.getItem("token")
+    // },
+    success: function (res) {
+        console.log(res);
+        if(res.status !== 0 && res.message ==="身份认证失败！"){
+            location.href = "/home/login.html"
+            return console.log("你出错了");
+        }
+        console.log(res);
+        let username = res.data.username;
+        let nickname = res.data.nickname;
+        let portraitSrc= res.data.user_pic;
+        let showname =  nickname||username;
+
+        $("#welcomeUser").text("欢迎:"+showname)
+        console.log($("#welcomeUser").text());
+
+        if(portraitSrc){
+            $(".text-portrait").hide();
+            $(".layui-nav-img").attr("src",portraitSrc);
+            $(".layui-nav-img").show()
+        }else{
+            $(".text-portrait").text(showname[0].toUpperCase());
+            $(".text-portrait").show();
+            $(".layui-nav-img").hide();
+        }
+    }
+})
+}
+
+getNameAndPortrait();
+
+// 退出
+let layer = layui.layer;
+$("#exitBtn").click((e)=>{
+    e.preventDefault();
+    layer.confirm('确定要退出?', {icon: 3, title:'提示'}, function(index){
+        //do something
+        localStorage.removeItem("token")
+        location.href="/home/login.html"
+        layer.close(index);
+      })
+})
+
